61af49660fe8d0f8ae71b5121eae7ba349d7bf00
Prajjwall123 <pokhrelprajwal29@gmail.com>
2024-12-20 13:58:58 +0545

    User model

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/db_projectsyeti");
+        mongoose.connect("mongodb://localhost:27017/db_projectsyeti");
         console.log("Connected to mongodb")
     } catch (e) {
         console.log(e)

2d642bea9726f1ce21c83bf3c332616937abef3e
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-01-30 15:00:26 +0545

    companymodel/02

--- model/Company.js
+++ model/Company.js
@@ -1,43 +1,14 @@
 const mongoose = require("mongoose");
 
 const companySchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true,
-        maxlength: 255
-    },
-    companyBio: {
-        type: String,
-        required: true,
-        maxlength: 255
-    },
-    employees: {
-        type: Number,
-        required: true
-    },
-    logo: {
-        type: String,
-        required: false,
-        maxlength: 255
-    },
-    projectsAwarded: {
-        type: Number,
-        default: 0
-    },
-    projectsPosted: {
-        type: Number,
-        default: 0
-    },
-    projectsCompleted: {
-        type: Number,
-        default: 0
-    }
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+    companyName: { type: String, maxlength: 255 },
+    companyBio: { type: String, maxlength: 255 },
+    employees: { type: Number },
+    logo: { type: String, maxlength: 255 },
+    projectsPosted: { type: Number, default: 0 },
+    projectsAwarded: { type: Number, default: 0 },
+    projectsCompleted: { type: Number, default: 0 },
 }, { timestamps: true });
 
 const Company = mongoose.model("Company", companySchema);

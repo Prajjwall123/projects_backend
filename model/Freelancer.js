8be5d5d785620a82aa95e499858810fc342d9e65
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-01-30 15:00:34 +0545

    freelancermodel/02

--- model/Freelancer.js
+++ model/Freelancer.js
@@ -1,39 +1,13 @@
 const mongoose = require("mongoose");
 
 const freelancerSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        unique: true
-    },
-    skills: {
-        type: [String],
-        required: true
-    },
-    experienceYears: {
-        type: Number,
-        required: true
-    },
-    profileImage: {
-        type: String
-    },
-    availability: {
-        type: String,
-        required: true
-    },
-    portfolio: {
-        type: String,
-        required: true
-    },
-    projectsCompleted: {
-        type: Number,
-        default: 0
-    },
-    profileImage: {
-        type: String,
-        default: null
-    }
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+    skills: { type: [String] },
+    experienceYears: { type: Number },
+    availability: { type: String },
+    portfolio: { type: String },
+    profileImage: { type: String },
+    projectsCompleted: { type: Number, default: 0 },
 });
 
 const Freelancer = mongoose.model("Freelancer", freelancerSchema);

95e84cee75868558716fb36d1c81768f8ebee1a0
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-01-30 15:01:01 +0545

    authroute/02

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const multer = require('multer');
+
 const router = express.Router();
-const UserValidation = require('../validation/userValidation');
 
-const { login, register, verifyOtp } = require("../controller/AuthController");
+const { login, register, verifyOtp, getUserProfile } = require('../controller/AuthController');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
 
-router.post('/register', UserValidation, register);
+const upload = multer({ storage });
+
+router.post('/register', upload.fields([
+    { name: 'logo', maxCount: 1 },
+    { name: 'profileImage', maxCount: 1 }
+]), register);
+
 router.post('/login', login);
 router.post('/verify-otp', verifyOtp);
 

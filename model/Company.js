0587a66abd02f57dccbc07b27388d53714dbebad
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-02-08 22:00:02 +0545

    update company profile

--- model/Company.js
+++ model/Company.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
 
-const companySchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
-    companyName: { type: String, maxlength: 255 },
-    companyBio: { type: String, maxlength: 255 },
-    employees: { type: Number },
-    logo: { type: String, maxlength: 255 },
-    projectsPosted: { type: Number, default: 0 },
-    projectsAwarded: { type: Number, default: 0 },
-    projectsCompleted: { type: Number, default: 0 },
-}, { timestamps: true });
+const companySchema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
+        companyName: { type: String, maxlength: 255 },
+        companyBio: { type: String, maxlength: 255 },
+        employees: { type: Number },
+        logo: { type: String, maxlength: 255 },
+        projectsPosted: { type: Number, default: 0 },
+        projectsAwarded: { type: Number, default: 0 },
+        projectsCompleted: { type: Number, default: 0 },
+        founded: { type: Number, min: 1900, max: new Date().getFullYear() },
+        ceo: { type: String, maxlength: 255 },
+        headquarters: { type: String, maxlength: 255 },
+        industry: { type: String, maxlength: 255 },
+        website: { type: String, maxlength: 255 },
+    },
+    { timestamps: true }
+);
 
 const Company = mongoose.model("Company", companySchema);
 

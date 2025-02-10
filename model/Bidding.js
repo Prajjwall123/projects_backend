c2e6c95ebdfb354b0989279e862c14ecadfca8ef
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-02-10 18:49:07 +0545

    bidding model

---
+++ model/Bidding.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const biddingSchema = new mongoose.Schema(
+    {
+        freelancer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Freelancer",
+            required: true,
+        },
+        project: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Project",
+            required: true,
+        },
+        amount: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        message: {
+            type: String,
+            maxlength: 1000,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const Bidding = mongoose.model("Bidding", biddingSchema);
+
+module.exports = Bidding;

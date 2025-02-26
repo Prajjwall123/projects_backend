a88f36d1fc3030c4a4b3967e112551fd69a6deb1
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-02-26 21:28:04 +0545

    testing/03- uploading image

--- test/project.test.js
+++ test/project.test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const { expect } = chai;
 const request = require('supertest');
 const server = require('../app.js');
+const path = require('path');
 
 describe('/First Test Collection', function () {
     it('test get all projects route...', function (done) {
@@ -11,6 +12,22 @@ describe('/First Test Collection', function () {
             .end((err, res) => {
                 if (err) return done(err);
                 expect(res.body).to.be.an('array');
+                done();
+            });
+    });
+});
+
+describe('/Auth API Tests', function () {
+
+    it('should upload an image and return the URL', function (done) {
+        request(server)
+            .post('/api/auth/upload')
+            .attach('image', path.join(__dirname, 'test-image.jpg'))
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.body).to.have.property('imageUrl');
+                expect(res.body.imageUrl).to.be.a('string');
                 done();
             });
     });
